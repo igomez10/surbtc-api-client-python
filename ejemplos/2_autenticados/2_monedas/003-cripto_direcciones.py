0fdc8dbdabc75eb9dcca32037a93f4d8d38a78d8
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-11-26 17:41:44 -0300

    Se agregan ejemplos de monedas (retiros y depósitos)

---
+++ ejemplos/2_autenticados/2_monedas/003-cripto_direcciones.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Cliente para servicios web de SURBTC
+Copyright (C) SASCO SpA (https://sasco.cl)
+
+Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+bajo los términos de la GNU Lesser General Public License (LGPL) publicada
+por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+o (a su elección) cualquier versión posterior de la misma.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD
+PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la GNU Lesser General
+Public License (LGPL) para obtener una información más detallada.
+
+Debería haber recibido una copia de la GNU Lesser General Public License
+(LGPL) junto a este programa. En caso contrario, consulte
+<http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+"""
+Ejemplo para obtener las direcciones de depósito para criptomonedas
+@link http://api.surbtc.com/#dep-sito-criptomonedas
+@author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+@version 2017-11-26
+"""
+
+# credenciales
+api_key = ''
+api_secret = ''
+
+# importar directamente del proyecto, sin instalar en el equipo, estas líneas no
+# son necesarias cuando se instala con PIP el cliente, ya que en ese caso el
+# módulo de surbtc quedará disponible en el sistema operativo
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+# importar módulo del cliente
+import surbtc
+
+# crear cliente y moneda
+Client = surbtc.Client(api_key, api_secret)
+Currency = Client.getCurrency('ETH')
+
+# crear depósito
+print(Currency.getAddresses())
---
+++ ejemplos/2_autenticados/2_monedas/005-cripto_retiro.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Cliente para servicios web de SURBTC
+Copyright (C) SASCO SpA (https://sasco.cl)
+
+Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+bajo los términos de la GNU Lesser General Public License (LGPL) publicada
+por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+o (a su elección) cualquier versión posterior de la misma.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD
+PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la GNU Lesser General
+Public License (LGPL) para obtener una información más detallada.
+
+Debería haber recibido una copia de la GNU Lesser General Public License
+(LGPL) junto a este programa. En caso contrario, consulte
+<http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+"""
+Ejemplo que muestra como realizar el retiro de criptomonedas
+@link http://api.surbtc.com/#nuevo-retiro
+@author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+@version 2017-11-26
+"""
+
+# credenciales
+api_key = ''
+api_secret = ''
+
+# importar directamente del proyecto, sin instalar en el equipo, estas líneas no
+# son necesarias cuando se instala con PIP el cliente, ya que en ese caso el
+# módulo de surbtc quedará disponible en el sistema operativo
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+# importar módulo del cliente
+import surbtc
+
+# crear cliente y moneda
+Client = surbtc.Client(api_key, api_secret)
+Currency = Client.getCurrency('ETH')
+
+# crear retiro
+target_address = '0x2a8535838d0d10E90ffcB36dd2E2788667B6c9C5'
+amount = 0.033030875
+print(Currency.withdrawal(target_address, amount))
