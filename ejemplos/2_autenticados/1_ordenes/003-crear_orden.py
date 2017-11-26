a4596ddf21f898629f525100e2a0e582668cd134
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-11-26 13:20:59 -0300

    Se agregan ejemplos de órdenes

---
+++ ejemplos/2_autenticados/1_ordenes/003-crear_orden.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Cliente para servicios web de SURBTC
+Copyright (C) SASCO SpA (https://sasco.cl)
+
+Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+bajo los términos de la GNU Lesser General Public License (LGPL) publicada
+por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+o (a su elección) cualquier versión posterior de la misma.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD
+PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la GNU Lesser General
+Public License (LGPL) para obtener una información más detallada.
+
+Debería haber recibido una copia de la GNU Lesser General Public License
+(LGPL) junto a este programa. En caso contrario, consulte
+<http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+"""
+Ejemplo para obtener las ordenes ejecutadas de un usuario en cierto mercado
+@link http://api.surbtc.com/#nueva-orden
+@author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+@version 2017-11-26
+"""
+
+# credenciales
+api_key = ''
+api_secret = ''
+
+# importar directamente del proyecto, sin instalar en el equipo, estas líneas no
+# son necesarias cuando se instala con PIP el cliente, ya que en ese caso el
+# módulo de surbtc quedará disponible en el sistema operativo
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+# importar módulo del cliente
+import surbtc
+
+# crear cliente y mercado
+Client = surbtc.Client(api_key, api_secret)
+Market = Client.getMarket('BTC-CLP')
+
+# crear una orden de compra para el usuario en el mercado
+# poco BTC y precio muy bajo para evitar comprar por error si se ejecuta el ejemplo sin modificar
+print(Market.createBuyOrder(0.0001, 1000)) # 0.0001 BTC a 1.000 pesos cada 1 BTC
+
+# crea una orden de compra a precio de mercado
+#print(Market.createBuyOrder(0.0001, price_type = 'market'))
+
+# crear una orden de venta para el usuario en el mercado
+# poco BTC y precio muy alto para evitar vender por error si se ejecuta el ejemplo sin modificar
+print(Market.createSellOrder(0.0001, 100000000)) # 0.0001 BTC a 100.000.000 pesos cada 1 BTC
+
+# crea una orden de venta a precio de mercado
+#print(Market.createSellOrder(0.0001, price_type = 'market'))
